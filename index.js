1d620414c5e8393cf9bbbe95e4a515169b779559
aditya thakor <aditya.github.27x@gmail.com>
2026-05-16 16:29:46 +0530

    add script for dynamic component inserting.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Team project");
+async function AddComponent(id,file) {
+    const res = await fetch(file);
+    const html = await res.text();
+
+    document.getElementById(id).innerHTML=html
+}
+
+AddComponent("header", "./src/modules/header/header.html")
+AddComponent("hero-section", "./src/modules/hero/hero.html")
+AddComponent("special-section", "./src/modules/specializing/specializing.html")
+AddComponent("about-section", "./src/modules/about/about.html")
+AddComponent("pricing-section", "./src/modules/pricing/pricing.html")
+AddComponent("contact-section", "./src/modules/contact/contact.html")
+AddComponent("logos-section", "./src/modules/logos/logos.html")
+AddComponent("footer", "./src/modules/footer/footer.html")
