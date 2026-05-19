35ee4ee5af558a327351ff169550aa51bf76fa75
solankiakshay <solankiakshay8980@gmail.com>
2026-05-19 13:48:07 +0530

    change js

---
+++ src/modules/hero/hero.js
@@ -0,0 +1,27 @@
+ <section class="sec1 position-relative">
+        <div class="container">
+            <div class="row" style="padding-top: 20%;">
+                <div class="col-lg-6">
+                    <div class="hero-content-wrapper">
+                        <h2 class="using">You're Using Free Lite Version</h2>
+
+                        <p class="using-p">Please purchase full version of the template to get all sections <br> and
+                            permission to use with commercial projects.</p>
+
+                        <button class="cssbuttons-io-button btn d-flex">
+                            Get started
+                            <div class="icon">
+                                <i class="fa-solid fa-angle-right"></i>
+                            </div>
+                        </button>
+                    </div>
+                </div>
+
+                <div class="col-lg-6">
+                    <div class="hero-img">
+                        <img src="https://themewagon.github.io/Nova-Bootstrap5_beta1/assets/img/hero/hero-5/hero-img.svg" alt="" class="img-fluid">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
